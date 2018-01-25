4c2b7c41867c7c90d7bc70100e8bdd0495b824ab
Adam Dominec <adominec@gmail.com>
2018-01-25 19:01:23 +0100

    initial camera app

---
+++ app/src/main/java/eu/dominec/adam/tracking/Tracking.java
@@ -0,0 +1,50 @@
+package eu.dominec.adam.tracking;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.WindowManager;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+
+public class Tracking extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final JavaCameraView view = new JavaCameraView(this.getApplicationContext(), 0);
+        view.setLayoutParams(new WindowManager.LayoutParams(WindowManager.LayoutParams.FILL_PARENT, WindowManager.LayoutParams.WRAP_CONTENT));
+        setContentView(view);
+        view.setVisibility(SurfaceView.VISIBLE);
+        view.setCvCameraViewListener(this);
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, new BaseLoaderCallback(this) {
+            @Override
+            public void onManagerConnected(int status) {
+                if (status == LoaderCallbackInterface.SUCCESS) {
+                    view.enableView();
+                } else {
+                    super.onManagerConnected(status);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        return inputFrame.rgba();
+    }
+}
