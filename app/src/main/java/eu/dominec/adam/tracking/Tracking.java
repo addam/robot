0b0b96097eea27a5e36c234c328544294c0c5640
Adam Dominec <adominec@gmail.com>
2018-01-25 19:12:43 +0100

    rotate to landscape, hide title

--- app/src/main/java/eu/dominec/adam/tracking/Tracking.java
+++ app/src/main/java/eu/dominec/adam/tracking/Tracking.java
@@ -1,9 +1,11 @@
 package eu.dominec.adam.tracking;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceView;
+import android.view.Window;
 import android.view.WindowManager;
 
 import org.opencv.android.BaseLoaderCallback;
@@ -14,10 +16,14 @@ import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Mat;
 
 public class Tracking extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         final JavaCameraView view = new JavaCameraView(this.getApplicationContext(), 0);
         view.setLayoutParams(new WindowManager.LayoutParams(WindowManager.LayoutParams.FILL_PARENT, WindowManager.LayoutParams.WRAP_CONTENT));
         setContentView(view);
