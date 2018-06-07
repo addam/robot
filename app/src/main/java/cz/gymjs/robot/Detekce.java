a7ebc032900c9e8e241e701dcea4a990bd1e086e
MagicSenten <mitka.honza@gmail.com>
2018-06-07 15:49:52 +0200

    detekujeme :)

--- app/src/main/java/cz/gymjs/robot/Detekce.java
+++ app/src/main/java/cz/gymjs/robot/Detekce.java
@@ -1,116 +1,71 @@
-import java.awt.image.BufferedImage;
-        import java.io.ByteArrayInputStream;
-        import java.io.InputStream;
-        import java.util.ArrayList;
-        import java.util.List;
+package cz.gymjs.robot;
 
-        import javax.imageio.ImageIO;
-        import javax.swing.ImageIcon;
-        import javax.swing.JFrame;
-        import javax.swing.JLabel;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
 
-        import org.opencv.calib3d.Calib3d;
-        import org.opencv.core.*;
-        import org.opencv.imgproc.Imgproc;
-        import org.opencv.video.Video;
-        import org.opencv.features2d.FeatureDetector;
-        import org.opencv.imgcodecs.Imgcodecs;
+import java.util.ArrayList;
+import java.util.List;
 
-        import static org.opencv.imgproc.Imgproc.*;
-        import static org.opencv.utils.Converters.vector_Point_to_Mat;
+import static org.opencv.imgproc.Imgproc.CHAIN_APPROX_SIMPLE;
+import static org.opencv.imgproc.Imgproc.COLOR_RGB2GRAY;
+import static org.opencv.imgproc.Imgproc.MORPH_DILATE;
+import static org.opencv.imgproc.Imgproc.MORPH_OPEN;
+import static org.opencv.imgproc.Imgproc.MORPH_RECT;
+import static org.opencv.imgproc.Imgproc.RETR_EXTERNAL;
+import static org.opencv.imgproc.Imgproc.getStructuringElement;
 
-public class optic {
-    public static void main(String[] args) {
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        Mat image = Imgcodecs.imread("C:/Users/mitka/Downloads/prazdno.jpg");
-        Mat lmaeft1 = Imgcodecs.imread("C:/Users/mitka/Downloads/plech.jpg");
-        System.out.println(lmaeft1.width()+"x"+lmaeft1.height());
-      
-        JFrame window = new JFrame();
-        JFrame window1 = new JFrame();
+public class Detekce {
+    public List<Point> plechovky = new ArrayList<Point>();
+    public List<Point> prekazka = new ArrayList<Point>();
+
+    public void detect(Mat mrizka, Mat fotka) {
         Mat vysledek = new Mat();
-        int l = 100;
-        int r = 130;
-        float a = 255/(r-l);
+        int l = 90;
+        int r = 110;
+        float a = 255 / (r - l);
         float b = l;
-        Core.subtract(lmaeft1, new Scalar(b,b,b), vysledek);
-        Core.multiply(vysledek, new Scalar(a,a,a), vysledek);
+        Core.subtract(fotka, new Scalar(b, b, b), vysledek);
+        Core.multiply(vysledek, new Scalar(a, a, a), vysledek);
         Mat kelner = getStructuringElement(MORPH_RECT, new Size(10, 10));
-        Core.absdiff(image,vysledek,vysledek);
+        Core.absdiff(mrizka, vysledek, vysledek);
         Imgproc.morphologyEx(vysledek, vysledek, MORPH_OPEN, kelner);
-        Imgproc.morphologyEx(vysledek, vysledek, MORPH_DILATE , kelner);
+        Imgproc.morphologyEx(vysledek, vysledek, MORPH_DILATE, kelner);
         List<MatOfPoint> contours = new ArrayList<>();
         Mat mat = new Mat();
         Imgproc.cvtColor(vysledek, vysledek, COLOR_RGB2GRAY);
-        Util.imshow(vysledek, window);
-        Imgproc.findContours(vysledek, contours,mat, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-        MatOfInt hulk = new MatOfInt();
+        Imgproc.findContours(vysledek, contours, mat, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
         MatOfPoint2f approxCurve = new MatOfPoint2f();
-        //For each contour found
-        for (int i=0; i<contours.size(); i++)
-        {
-            //Convert contours(i) from MatOfPoint to MatOfPoint2f
-            MatOfPoint2f contour2f = new MatOfPoint2f( contours.get(i).toArray() );
-            //Processing on mMOP2f1 which is in type MatOfPoint2f
-            double approxDistance = Imgproc.arcLength(contour2f, true)*0.02;
+        for (int i = 0; i < contours.size(); i++) {
+            MatOfPoint2f contour2f = new MatOfPoint2f(contours.get(i).toArray());
+            double approxDistance = Imgproc.arcLength(contour2f, true) * 0.02;
             Imgproc.approxPolyDP(contour2f, approxCurve, approxDistance, true);
-
-            //Convert back to MatOfPoint
-            MatOfPoint points = new MatOfPoint( approxCurve.toArray() );
-            // Get bounding rect of contour
-            Rect rect = Imgproc.boundingRect(points);
-            List<Point> plechovky = new ArrayList<Point>();
-            int x = bounds.width/2;
-    		double y = bounds.height*0.2;
-    		plechovky.add(new Point( x, y));
+            MatOfPoint points = new MatOfPoint(approxCurve.toArray());
+            Rect bounds = Imgproc.boundingRect(points);
             int pointCount = points.toList().size();
-            if (pointCount >= 4 ) {
-            	Rect bounds = Imgproc.boundingRect(points);
-            	float ar = bounds.width/(float)bounds.height;
-            	System.out.println(bounds.width);
-            	System.out.println(bounds.height);
-            	System.out.println(ar);
-            	if (ar >= 0.50 & ar <= 0.90){
-            		System.out.println("plechovka");
-            	} else {
-            			System.out.println("nepratesky robot");
-            	}
+            if (pointCount >= 4) {
+                float ar = bounds.width / (float) bounds.height;
+                if (ar >= 0.50 & ar <= 0.80) {
+                    System.out.println("plechovka");
+                    int x = bounds.x + bounds.width / 2;
+                    double y = bounds.y + bounds.height * 0.2;
+                    plechovky.add(new Point(x, y));
+                } else {
+                    int x = bounds.x + bounds.width / 2;
+                    double y = bounds.y + bounds.height * 0.2;
+                    prekazka.add(new Point(x, y));
+                }
             } else {
-        		System.out.println("neni obdelnik");
-        	}
-
-             // draw enclosing rectangle (all same color, but you could use variable i to make them unique)
-            Imgproc.rectangle(image, rect.tl(),  rect.br(), new Scalar(255, 0, 0),1, 8,0);
-      
-
-        }
-        Util.imshow(image, window1);
-    }
-
-	private static Scalar Scalar(int i, int j, int k) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-}
-
-class Util {
-
-
-    public static void imshow(Mat img, JFrame frame) {
-        MatOfByte matOfByte = new MatOfByte();
-        Imgcodecs.imencode(".png", img, matOfByte);
-        byte[] byteArray = matOfByte.toArray();
-        BufferedImage bufImage = null;
-        try {
-            InputStream in = new ByteArrayInputStream(byteArray);
-            bufImage = ImageIO.read(in);
-            frame.getContentPane().removeAll();
-            frame.getContentPane().add(new JLabel(new ImageIcon(bufImage)));
-            frame.pack();
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setVisible(true);
-        } catch (Exception e) {
-            e.printStackTrace();
+                int x = bounds.x + bounds.width / 2;
+                double y = bounds.y + bounds.height * 0.2;
+                prekazka.add(new Point(x, y));
+            }
         }
     }
 }
