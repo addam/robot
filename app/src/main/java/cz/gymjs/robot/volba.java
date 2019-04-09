2160cf833248cb635c5b2164839cf9111d4acacc
vojta ulman <vojta.ulman@gmail.com>
2019-04-09 12:40:41 +0100

    začleňování  intentů

---
+++ app/src/main/java/cz/gymjs/robot/volba.java
@@ -0,0 +1,24 @@
+package cz.gymjs.robot;
+
+/* void setobtiznost(){
+        Intent callingIntent=getIntent();
+        obtiznost=callingIntent.getIntentExtra("name",0)
+        }
+        void jizdadopredu(){
+        Intent i=new Intent(this,MainActivity.class);
+        i.putExtra("name",1);
+        startActivity(i);
+
+        void jizdadozadu(){
+        Intent i=new Intent(this,MainActivity.class);
+        i.putExtra("name",2);
+        startActivity(i);
+
+
+        }
+
+public class volba {
+
+}
+*/
+
